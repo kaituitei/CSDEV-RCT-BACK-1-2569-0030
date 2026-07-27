54c090d988d5b2285eb9c49a2ad27cc44792c32f
KILLVader8550 <KILLVader8550@gmail.com>
2026-07-27 18:14:53 +0700

    chore: edit line format

--- src/middlewares/authCheck.ts
+++ src/middlewares/authCheck.ts
@@ -2,7 +2,7 @@ import type { Context, Next } from 'hono'
 import { verify } from 'hono/jwt';
 
 export async function authCheck(c: Context, next: Next) {
-	const authHeader = await c.req.header('Authorization');
+	const authHeader = c.req.header('Authorization');
 
 	if (!authHeader || !authHeader.startsWith('Bearer '))
 		return (c.json({ error: 'Unauthorized' }, 401));
@@ -13,7 +13,7 @@ export async function authCheck(c: Context, next: Next) {
 
 	const user =  await verify(token!, process.env.JWT_SECRET!, 'HS256');
 	if (!user)
-		return (c.json({ error: 'Invalid token'}, 401));
+		return (c.json({ error: 'Invalid token' }, 401));
 
 	c.set('user', user);
 	await next();
