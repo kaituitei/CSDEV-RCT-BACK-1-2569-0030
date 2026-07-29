a18dfbb0abed4a7220daaa991e459f9509c3dc55
KILLVader8550 <KILLVader8550@gmail.com>
2026-07-29 09:50:55 +0700

    refactor: Remove try-catch block

--- src/api/items/items.getId.ts
+++ src/api/items/items.getId.ts
@@ -4,18 +4,12 @@ import { getNoticeById } from "../../services/notice.service.js";
 const getById = new Hono();
 
 getById.get('/:id', async (c) => {
-	try {
-		const id = c.req.param('id');
+	const id = c.req.param('id');
 
-		const notice = await getNoticeById(id);
-		if (!notice)
-			return (c.json({ "status": "error", "error": 'Not found notice with this ID' }, 201));
-		return (c.json({ "status": "success", "data": notice }, 200));
-	}
-	catch (error) {
-		console.error(error);
-		return (c.json({ "status": "error", "error": "Internal server fail" }, 500));
-	}
+	const notice = await getNoticeById(id);
+	if (!notice)
+		return (c.json({ "status": "error", "error": 'Not found notice with this ID' }, 201));
+	return (c.json({ "status": "success", "data": notice }, 200));
 });
 
 export default (getById);
