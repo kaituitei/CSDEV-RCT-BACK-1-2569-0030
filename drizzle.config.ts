d7c5d87d64b5bdf8f9037b587eacccac328e553f
KILLVader8550 <KILLVader8550@gmail.com>
2026-07-30 10:11:42 +0700

    refactor: use neon db instead

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "drizzle-kit";
+import { config } from "dotenv";
+
+config({ path: '.env.local' })
 
 export default defineConfig({
-	out: './drizzle',
+	out: './migrations',
 	dialect: 'postgresql',
 	schema: './src/db/schema',
 	dbCredentials: {
 		url: process.env.DATABASE_URL!,
 	}
-})
+});
--- src/db/index.ts
+++ src/db/index.ts
@@ -3,6 +3,8 @@ import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import { relations } from './relations.js';
 
-config({ path: '.env'})
+config({ path: '.env.local'})
 
-export const postgres = drizzle(process.env.DATABASE_URL!, { relations });
+const sql = neon(process.env.DATABASE_URL!);
+
+export const postgres = drizzle({client: sql, relations });
